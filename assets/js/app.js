42e4c13fdea74a6d7bbe74317ec31452e40a695d
thomasmoser <moser.thm@gmail.com>
2023-05-18 19:20:18 +0200

    project wtm fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1419,11 +1419,11 @@ function horizontalLoop(items, config) {
   items = gsap.utils.toArray(items);
   config = config || {};
   let tl = gsap.timeline({
-      repeat: config.repeat,
-      paused: config.paused,
-      defaults: { ease: "none" },
-      onReverseComplete: () => tl.totalTime(tl.rawTime() + tl.duration() * 100),
-    }),
+    repeat: config.repeat,
+    paused: config.paused,
+    defaults: { ease: "none" },
+    onReverseComplete: () => tl.totalTime(tl.rawTime() + tl.duration() * 100),
+  }),
     length = items.length,
     startX = items[0].offsetLeft,
     times = [],
